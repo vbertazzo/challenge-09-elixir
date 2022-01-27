6c33ef0f04de7a6d668e8f8a9e6b56993b7d1434
vbertazzo <vbertazzo@gmail.com>
2022-01-27 14:48:17 -0300

    Add user struct

--- lib/exmeal/user.ex
+++ lib/exmeal/user.ex
@@ -1,20 +1,43 @@
 defmodule Exmeal.User do
   use Ecto.Schema
-
   import Ecto.Changeset
-
-  @derive {Jason.Encoder, only: [:cpf, :id, :email, :name]}
-  @foreign_key_type :binary_id
+  alias Exmeal.Meal
 
   @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
 
   @required_params [:cpf, :email, :name]
 
+  @derive {Jason.Encoder, only: [:cpf, :id, :email, :name]}
+
   schema "users" do
-    # TO DO
+    field :name, :string
+    field :cpf, :string
+    field :email, :string
+
+    has_many :meals, Meal
+
+    timestamps()
   end
 
-  def changeset() do
-    # TO DO
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_params()
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_params()
+  end
+
+  defp validate_params(struct) do
+    struct
+    |> validate_required(@required_params)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cpf, is: 11)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
   end
 end
